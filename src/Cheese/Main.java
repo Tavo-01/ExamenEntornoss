debca380af27ea70999b5984b9bee063c08106d8
Tavo-01 <gusigus2002@gmail.com>
2022-06-09 18:37:49 +0200

    COMMIT master MAIN

--- src/Cheese/Main.java
+++ src/Cheese/Main.java
@@ -17,8 +17,15 @@ public class Main {
 	 * @author ___
 	 */
      public static void main(String[] args) {
-    
+    	 //String codigo,String tipo, int cantidad, double peso
+    	 Producto primero = new Producto("Papel","Higiene",10,45.5);
+    	 Producto segundo = new Producto("Gel hidroalcoholico","Higiene",6,6);
+    	 Producto tercero = new Producto("Paracetamol","Analgesico",3,2.5);
+    	 primero.imprime_etiqueta();
+    	 segundo.imprime_etiqueta();
+    	 tercero.imprime_etiqueta();
          System.out.println("1º Gdam: Entornos de desarrollo");
+         
 
      }
 }
